0f087a5accfc93f4596dd9d775f62ac1b0478be2
rayhan926 <rayhanbd7075@gmail.com>
2021-11-08 13:33:31 +0600

    Added swiper in header tags

--- components/Header/HeaderTags.js
+++ components/Header/HeaderTags.js
@@ -10,11 +10,23 @@ import {
 import HeaderTagSkelenton from "../Skeleton/HeaderTagSkelenton";
 import request from "./../../api";
 
+import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Mousewheel, Pagination } from "swiper";
+import { useRef } from "react";
+
+SwiperCore.use([Navigation, Mousewheel, Pagination]);
+
 function HeaderTags({ className }) {
   const dispatch = useDispatch();
   const [videoCategories, setVideoCategories] = useState(null);
   const [loading, setLoading] = useState(true);
   const [active, setActive] = useState("");
+
+  const nextElm = useRef(null)
+  const prevElm = useRef(null)
+
+
   const handleClick = (tag) => {
     setActive(tag);
     dispatch(emptyVideos())
@@ -51,27 +63,57 @@ function HeaderTags({ className }) {
 
   return (
     <div
-      className={`sm:px-5 flex space-x-3 items-center py-3 border-b select-none border-gray-200 w-full max-w-full overflow-x-auto thin_scrollbar ${className}`}
+      className={`sm:px-5 py-3 relative border-b select-none border-gray-200 w-full max-w-full ${className}`}
     >
       {videoCategories && !loading
-        ? videoCategories.map((category, i) => (
-          <div
-            key={i}
-            onClick={() =>
-              handleClick(category?.snippet?.title.replace("/", " "))
-            }
-            className={`rounded-full text-sm whitespace-nowrap text-gray-900 py-[6px] hover:bg-gray-200 px-3.5 cursor-pointer ${active === category?.snippet?.title ||
-              (!active && category?.snippet?.title === "All")
-              ? "bg-black text-white hover:bg-gray-900"
-              : "border border-gray-300 bg-gray-100"
-              }`}
-          >
-            {category?.snippet?.title.replace("/", " ")}
-          </div>
-        ))
-        : [...Array(15)].map((e, i) => <HeaderTagSkelenton key={i} />)}
+        ? (
+          <>
+            <Swiper
+              mousewheel={true}
+              direction={'horizontal'}
+              slidesPerView='auto'
+              spaceBetween={15}
+              pagination={{
+                "clickable": true
+              }}
+              onInit={(swiper) => {
+                swiper.params.navigation.prevEl = prevElm.current;
+                swiper.params.navigation.nextEl = nextElm.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }}
+            >
+              <button className="swiper_navigation_button prev_button left-0 justify-start" ref={prevElm}><BsChevronLeft className="w-5 h-5 text-gray-700" /></button>
+              <button className="swiper_navigation_button next_button right-0 justify-end" ref={nextElm}><BsChevronRight className="w-5 h-5 text-gray-700" /></button>
+              {
+                videoCategories.map((category, i) => (
+                  <SwiperSlide key={i} style={{ width: 'auto' }}
+                    onClick={() =>
+                      handleClick(category?.snippet?.title.replace("/", " "))
+                    }>
+                    <div
+                      className={`rounded-full text-sm whitespace-nowrap text-gray-900 py-[6px] hover:bg-gray-200 px-3.5 cursor-pointer ${active === category?.snippet?.title ||
+                        (!active && category?.snippet?.title === "All")
+                        ? "bg-black text-white hover:bg-gray-900"
+                        : "border border-gray-300 bg-gray-100"
+                        }`}
+                    >
+                      {category?.snippet?.title.replace("/", " ")}
+                    </div>
+                  </SwiperSlide>
+                ))
+              }
+            </Swiper>
+          </>
+        )
+        : <div className="flex items-center space-x-2" >
+          {[...Array(15)].map((e, i) => <HeaderTagSkelenton key={i} />)}
+        </div>}
     </div>
   );
 }
 
 export default HeaderTags;
+
+
+
--- pages/test.js
+++ pages/test.js
@@ -4,26 +4,46 @@
 
 // const Swiper = dynamic(() => import("swiper/react").then(mod => mod.Swiper));
 // const SwiperSlide = dynamic(() => import("swiper/react").then(mod => mod.SwiperSlide));
-
+import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Mousewheel, Pagination } from "swiper";
+import { useRef } from "react";
 
 SwiperCore.use([Navigation, Mousewheel, Pagination]);
 
 function Test() {
+
+    const nextElm = useRef(null)
+    const prevElm = useRef(null)
+
     return (
-        <div className="w-[700px] mt-[80px] mx-auto border border-green-100">
+        <div className="w-[900px] mt-[80px] mx-auto border border-green-100 relative">
             <Swiper
                 mousewheel={true}
                 direction={'horizontal'}
-                slidesPerView={1}
+                slidesPerView='auto'
+                spaceBetween={15}
                 pagination={{
                     "clickable": true
                 }}
+                onInit={(swiper) => {
+                    swiper.params.navigation.prevEl = prevElm.current;
+                    swiper.params.navigation.nextEl = nextElm.current;
+                    swiper.navigation.init();
+                    swiper.navigation.update();
+                }}
             >
+                <button className="swiper_navigation_button prev_button left-0 justify-start" ref={prevElm}><BsChevronLeft className="w-6 h-6 text-gray-700" /></button>
+                <button className="swiper_navigation_button next_button right-0 justify-end" ref={nextElm}><BsChevronRight className="w-6 h-6 text-gray-700" /></button>
                 {
-                    [...new Array(10)].map((i, e) => (
-                        <SwiperSlide key={e}>Slide {e}</SwiperSlide>
+                    [...new Array(30)].map((i, e) => (
+                        <SwiperSlide key={e} style={{ width: 'auto' }}>
+                            <div
+                                className={`rounded-full text-sm whitespace-nowrap text-gray-900 py-[6px] hover:bg-gray-200 px-3.5 cursor-pointer border border-gray-300 bg-gray-100`}
+                            >
+                                Category
+                            </div>
+                        </SwiperSlide>
                     ))
                 }
             </Swiper>
